982a075bfda94e4f3daaa386e9e2bd6e5ab5c709
Muhammad Haekal Sutrisna <78276395+haekalsutrisna@users.noreply.github.com>
2024-09-04 10:39:54 +0700

    update

--- game.js
+++ game.js
@@ -7,70 +7,53 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
+alert("Welcome to Simon Game Apps")
+alert("Quick Tips Before You Start! You Have to clicked from the start again after every level so remember the sequence is KOENTJI to Menang");
+
 $(document).keypress(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
-  }
+  };
 });
 
 $(".btn").click(function() {
-
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-
   playSound(userChosenColour);
   animatePress(userChosenColour);
-
   checkAnswer(userClickedPattern.length-1);
 });
 
-function gameoverTrigger () {
-  $("body").toggleClass("game-over")
-};
 
 function checkAnswer(currentLevel) {
-
     if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
-
       console.log("success");
       if (gamePattern.length === userClickedPattern.length){
         setTimeout(function () {
           nextSequence();
         }, 1000);
-
       }
-
     } else {
-
       console.log("wrong");
       playSound("wrong");
-
       $("body").addClass("game-over");
       setTimeout(function () {
         $("body").removeClass("game-over");
       }, 200);
-
-      $("#level-title").text("Game Over, Press Any Key to Restart");
-      
+      $("#level-title").text("Game Over, Press Any Key on Keyboard to Restart");
       startOver();
-
     };
-
 }
 
 function nextSequence() {
-
   userClickedPattern = [];
-
   level++;
   $("#level-title").text("Level " + level);
-
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
 }
@@ -92,4 +75,4 @@ function startOver(){
   gamePattern = [];
   started = false;
   console.log("Game reset. You can start over.");
-}
+};
